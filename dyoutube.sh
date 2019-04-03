29acfe77aedc29e344cb5df234a0bba4bbdec607
Allen Liu <lhard@qq.com>
2019-04-03 23:46:48 +0800

    optimize

--- dyoutube.sh
+++ dyoutube.sh
@@ -7,11 +7,16 @@ then
   currentDate=$(date +%m%d-%H%M%S)
   if [ "$2" = "$TYPE" ]
   then
-    /usr/local/bin/youtube-dl --format mp4 $1 -o "~/OneDrive/video/${currentDate}.%(ext)s"
+    /usr/local/bin/youtube-dl --format mp4 "$1" -o "$HOME/OneDrive/video/${currentDate}.%(ext)s"
   else
-    /usr/local/bin/youtube-dl -x --audio-format m4a $1 -o "~/OneDrive/video/${currentDate}.%(ext)s"
+    /usr/local/bin/youtube-dl -x --audio-format m4a "$1" -o "$HOME/OneDrive/video/${currentDate}.%(ext)s"
   fi
-  /usr/local/bin/onedrive --synchronize  
+
+  if [ "$3" != "n" ]
+  then
+    /usr/local/bin/onedrive --synchronize
+  fi
+
   echo 'Everything is OK!'
 else
   echo "URL is empty"
